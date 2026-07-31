3ffee0df3e8f659598bb7bbaa9815c9e2c09a30e
Aleyna <anatoliasource@gmail.com>
2026-07-31 12:47:20 +0300

    Add_fetcher_and_d1_types

--- types/cloudflare-workers.d.ts
+++ types/cloudflare-workers.d.ts
@@ -3,3 +3,17 @@ declare module 'cloudflare:workers' {
   const _default: typeof env;
   export default _default;
 }
+
+// Minimal declarations for Cloudflare-specific runtime types used in the project
+interface Fetcher {
+  fetch(input: RequestInfo, init?: RequestInit): Promise<Response>;
+}
+
+interface D1Database {
+  prepare(query: string): {
+    all(...args: any[]): Promise<any[]>;
+    run(...args: any[]): Promise<any>;
+    first(...args: any[]): Promise<any>;
+  };
+}
+
